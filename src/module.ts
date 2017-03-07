e7c965cf0ce186331bbe8a543b79ae7e66a302f2
FlyingDonkey <jsosa@flyingdonkey.es>
2017-03-07 21:38:42 +0100

    Agregado ionic-image-loader

--- src/module.ts
+++ src/module.ts
@@ -4,13 +4,15 @@ import { IonicModule } from 'ionic-angular';
 import { IonAlphaScroll } from './ion-alpha-scroll';
 import { UtilModule } from './util-module';
 import { DynamicComponentModule } from 'ng-dynamic';
+import { IonicImageLoader } from 'ionic-image-loader';
 
 @NgModule({
   imports: [
     IonicModule,
+    IonicImageLoader,
     CommonModule,
     DynamicComponentModule.forRoot({
-      imports: [CommonModule, IonicModule, UtilModule],
+      imports: [CommonModule, IonicModule, UtilModule, IonicImageLoader],
       declarations: []
     })
   ],
